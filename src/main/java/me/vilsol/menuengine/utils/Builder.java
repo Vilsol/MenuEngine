67896613ab8c150d0131946fc64d565803a9da17
Vilsol <vilsol2000@gmail.com>
2015-03-07 04:37:44 +0000

    Change builder in to a builder pattern. Deprecate old methods.

--- src/main/java/me/vilsol/menuengine/utils/Builder.java
+++ src/main/java/me/vilsol/menuengine/utils/Builder.java
@@ -1,10 +1,10 @@
 package me.vilsol.menuengine.utils;
 
-import java.util.List;
-
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.List;
 
 public class Builder {
 	
@@ -14,37 +14,67 @@ public class Builder {
 		item = new ItemStack(material);
 	}
 
-	public Builder setDurability(short durability){
-		item.setDurability(durability);
-		return this;
-	}
-	
-	public Builder setType(Material material){
-		item.setType(material);
-		return this;
-	}
-	
-	public Builder setAmount(int amount){
-		item.setAmount(amount);
-		return this;
-	}
-	
-	public Builder setName(String name){
-		ItemMeta m = item.getItemMeta();
-		m.setDisplayName(name);
-		item.setItemMeta(m);
-		return this;
-	}
-	
-	public Builder setLore(List<String> lore){
-		ItemMeta m = item.getItemMeta();
-		m.setLore(lore);
-		item.setItemMeta(m);
-		return this;
-	}
-	
-	public ItemStack getItem(){
-		return item;
-	}
+    public Builder durability(short durability){
+        item.setDurability(durability);
+        return this;
+    }
+
+    public Builder type(Material material){
+        item.setType(material);
+        return this;
+    }
+
+    public Builder amount(int amount){
+        item.setAmount(amount);
+        return this;
+    }
+
+    public Builder name(String name){
+        ItemMeta m = item.getItemMeta();
+        m.setDisplayName(name);
+        item.setItemMeta(m);
+        return this;
+    }
+
+    public Builder lore(List<String> lore){
+        ItemMeta m = item.getItemMeta();
+        m.setLore(lore);
+        item.setItemMeta(m);
+        return this;
+    }
+
+    public ItemStack item(){
+        return item;
+    }
+
+    @Deprecated
+    public Builder setDurability(short durability){
+        return durability(durability);
+    }
+
+    @Deprecated
+    public Builder setType(Material material){
+        return type(material);
+    }
+
+    @Deprecated
+    public Builder setAmount(int amount){
+        return amount(amount);
+    }
+
+    @Deprecated
+    public Builder setName(String name){
+        return name(name);
+    }
+
+    @Deprecated
+    public Builder setLore(List<String> lore){
+        return lore(lore);
+    }
+
+    @Deprecated
+    public ItemStack getItem(){
+        return item();
+    }
 	
 }
